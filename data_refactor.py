7673caecc86d1c65cc32f8a736a5fd0a66e1c94c
OoiJunHao <ooi_jun_hao@yahoo.com.sg>
2023-03-24 23:15:30 +0800

    add data refactoring script

---
+++ data_refactor.py
@@ -0,0 +1,10 @@
+"""
+Since all the scripts output the full data in a csv format, it can become
+very time and space consuming when running high levels of landscape_repititions*agent_number*search_iterations.
+
+Use this script to convert the full csv files into compressed bz2 files, as well as extract out the data that is of interest.
+
+Or, go to the script and edit the csv output portion to include a compression technique.
+
+Edit names of files and reformatting logic as required.
+"""
