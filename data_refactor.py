15d0724ccefef853bb4e35209ab27d2d7ee0218d
Ooi Jun Hao <60221657+OoiJunHao@users.noreply.github.com>
2023-03-24 23:16:29 +0800

    Update data_refactor.py

--- data_refactor.py
+++ data_refactor.py
@@ -8,3 +8,26 @@ Or, go to the script and edit the csv output portion to include a compression te
 
 Edit names of files and reformatting logic as required.
 """
+import pandas as pd
+
+# Edit names to paths of interest
+file_names = ["availability__50__together", "availability__70__together", "availability__100__together", "availability__50__separate", "availability__70__separate", "availability__100__separate"]
+
+for file_name in file_names:
+    #---read-full-name---
+    df = pd.read_csv(f"{file_name}.csv", header=None)
+    print(df.size)
+    print(df.head())
+
+    #---out-compressed file---
+    df.to_csv(f"{file_name}.bz2" , index=False, compression='bz2')
+
+
+    #---load---
+    result_read = pd.read_csv(f"{file_name}.bz2", compression='bz2')
+    print(result_read.size)
+    print(result_read.head())
+
+    #--obtain last column values---
+    values = result_read[result_read.columns[-1]]
+    pd.DataFrame(values).to_csv(f"{file_name}_final_values.csv", header=False)
